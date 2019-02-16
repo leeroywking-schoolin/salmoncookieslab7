162225932e0c7cc2a7518e997dbdd99f663bb241
Lee-Roy King <leeroywking@gmail.com>
2019-02-16 11:54:13 -0800

    got my table working

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 'use strict';
 
 var hours = [
-  '6am',
-  '7am',
-  '8am',
-  '9am',
-  '10am',
-  '11am',
-  '12am',
-  '1pm',
-  '2pm',
-  '3pm',
-  '4pm',
-  '5pm',
-  '6pm',
-  '7pm',
-  '8pm'
+  '6:00am',
+  '7:00am',
+  '8:00am',
+  '9:00am',
+  '10:00am',
+  '11:00am',
+  '12:00am',
+  '1:00pm',
+  '2:00pm',
+  '3:00pm',
+  '4:00pm',
+  '5:00pm',
+  '6:00pm',
+  '7:00pm',
+  '8:00pm'
 ];
 
 
@@ -24,40 +24,61 @@ var seaTac = document.getElementById('seatac');
 var seattleCenter = document.getElementById('seacenter');
 var caphill = document.getElementById('caphill');
 var alki = document.getElementById('alkibeach');
+var tablehead = document.getElementById('tablehead');
 
 // You need to pass this constructor function the max, min and cookie per sale, also the htmlElement from above ie var locationObject = new LocationConstructor(100,30,3.5,html id));
 
-function LocationConstructor(max,min,cookiepersale,htmlElementId) {
+function LocationConstructor(max,min,cookiepersale,htmlElementId,rowTitle) {
   this.maxCustPerHour = max;
   this.minCustPerHour = min;
   this.cookiesPerSale = cookiepersale;
+  this.rowTitle = rowTitle;
   this.randRange = function(){
     return Math.floor((Math.random() *(this.maxCustPerHour - this.minCustPerHour)) + this.minCustPerHour)};
   this.cookiesPerHour= this.numCustomerPerHour * this.cookiesPerSale
   this.render= function () {
     var total = 0
+    var rowTitleRend = document.createElement('td');
+    rowTitleRend.textContent = this.rowTitle;
+    htmlElementId.appendChild(rowTitleRend);
     for (var i = 0; i < hours.length; i++) {
-      var liEl = document.createElement('li');
+      var liEl = document.createElement('td');
       var numCustomerPerHour = this.randRange();
       var cookiesPerHour = Math.floor(numCustomerPerHour * this.cookiesPerSale);
-      liEl.textContent = `${hours[i]}: ${cookiesPerHour} cookies.`;
+      liEl.textContent = `${cookiesPerHour} cookies.`;
       htmlElementId.appendChild(liEl);
       total = cookiesPerHour + total;
     }
-    var liEltotal = document.createElement('li');
+    var liEltotal = document.createElement('td');
     liEltotal.textContent = `Total: ${total} cookies.`;
     htmlElementId.appendChild(liEltotal);
   }
 };
 
-var firstAPObject = new LocationConstructor(65,23,6.3,firstAndPike);
-var seaTacObject = new LocationConstructor(24,2,1.2,seatac);
-var seaCentObject = new LocationConstructor(38,11,3.7,seacenter);
-var capHillObject = new LocationConstructor(38,20,2.3,caphill);
-var alkiBeachObject = new LocationConstructor(16,2,4.6,alkibeach);
+var firstAPObject = new LocationConstructor(65,23,6.3,firstAndPike,'First and Pike');
+var seaTacObject = new LocationConstructor(24,2,1.2,seatac, 'SeaTac Airport');
+var seaCentObject = new LocationConstructor(38,11,3.7,seacenter,'Seattle Center');
+var capHillObject = new LocationConstructor(38,20,2.3,caphill,'Capitol Hill');
+var alkiBeachObject = new LocationConstructor(16,2,4.6,alkibeach,'Alki');
 
 firstAPObject.render();
 seaTacObject.render();
 seaCentObject.render();
 capHillObject.render();
 alkiBeachObject.render();
+
+function tableHeadRender(){
+  var blank = document.createElement('th');
+  blank.innerHTML = ('');
+  tablehead.appendChild(blank);
+  for (var hourCount = 0; hourCount < hours.length; hourCount++){
+    var tableHeadRow = document.createElement('th');
+    tableHeadRow.textContent = hours[hourCount];
+    tablehead.appendChild(tableHeadRow)
+  }
+  var totals = document.createElement('th');
+  totals.innerHTML = ('Totals');
+  tablehead.appendChild(totals);
+};
+
+tableHeadRender();
